5e8a16b091c647721f993291f9f384f5f7e58995
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-07-28 22:51:03 +0200

    setup dbconnection to mongoDB and require all newly downloaded dependencys

--- server/app.js
+++ server/app.js
@@ -1,20 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const config = require("dotenv").config();
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+app.use(cors());
+
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@gameover-uogeo.mongodb.net/test?retryWrites=true&w=majority`);
+
+const db = mongoose.connection;
+db.on("error", function() {
+  console.error("Whoops, something went wrong!");
+});
+db.once("open", function() {
+  console.log("DB connection is up and running");
+});
+
 
 // view engine setup
+app.set("view engine", "pug");
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
