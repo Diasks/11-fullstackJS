9718907ce7ca394b35ec97348219cbbe9bfe9320
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-07-28 22:44:56 +0200

    Add model for userswith basic schema

---
+++ server/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+_id: mongoose.Schema.Types.ObjectId,
+firstname: String,
+lastname: String,
+birth: Number,
+email: String,
+password: String
+});
+
+
+module.exports = mongoose.model("User", UserSchema);
