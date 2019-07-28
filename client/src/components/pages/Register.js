49a3d0d845c0c0f0d128566e0c4e1f2dc1645a8e
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-07-28 15:49:00 +0200

    add inputs to field

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -8,10 +8,15 @@ render() {
 <h1>Register</h1>
 <form>
 <label>Firstname</label>
+<input type="text" placeholder="Firstname"/>
 <label>Lastname</label>
+<input type="text" placeholder="Lastname"/>
 <label>Date of birth</label>
+<input type="number" placeholder="Birth"/>
 <label>Email</label>
+<input type="email" placeholder="Email"/>
 <label>Password</label>
+<input type="password" placeholder="password"/>
 </form>
 </div>
 </div>
