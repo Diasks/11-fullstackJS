bd3249f351194c4d2e3200713dace4aeb4707dc0
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-07-28 15:43:21 +0200

    add register component

---
+++ client/src/components/pages/Register.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+render() {
+    return (
+<div>
+<div>
+<h1>Register</h1>
+<form>
+<label>Firstname</label>
+<label>Lastname</label>
+<label>Date of birth</label>
+<label>Email</label>
+<label>Password</label>
+</form>
+</div>
+</div>
+
+    );
+}
+}
+
+export default Register;
