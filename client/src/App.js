9c133fd8d368105cc23677faf5d0c8c5c5c3d702
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-07-31 21:44:44 +0200

    make route for login

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
     <Router>
         <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
     </Router>
     
   );
