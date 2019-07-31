7ff6fab2481acf461879f4bf4c24e3923ab88d24
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-07-31 15:37:50 +0200

    starting to build in jwt when creating user

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require("dotenv").config();
 
-router.use(bodyParser.urlencoded( { extended: true } ));
+router.use(bodyParser.urlencoded( { extended: false } ));
 router.use(bodyParser.json());
 
 const User = require('../models/User');
@@ -16,12 +19,16 @@ const hashedPassword = bcrypt.hashSync(req.body.password, 10);
     lastname: req.body.lastname,
     birtdate: req.body.birthdate,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
     }, function (error, user){
         if(error){
             return res.status(500).send("and error occured")
         } else {
-            res.status(200).send(user);
+            //Create a jwt token
+            let token = jwt.sign({id: user._id}, process.env.SECRET, {
+                expiresIn: 86400 //valid 24 hours
+            });
+            res.status(200).send({authenticated: true, token: token});
         }
     });
     });
