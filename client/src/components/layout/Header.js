f2d43cf5ba4cdf5dc43a77a0465d71d3dff90fe6
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-08-01 19:55:55 +0200

    import fontawesome icons, import styled components, write basic styled components for header, make a prototype without any function for now

---
+++ client/src/components/layout/Header.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const NavWrap = styled.div`
+width: 100%
+border-bottom: 1px solid black;
+display: flex;
+background-color: lightgrey;
+`;
+
+const IconWrapSearch = styled.div`
+padding-top: 12px;
+margin: 2px;
+margin-right: 10px;
+`;
+
+const IconWrap = styled.div`
+padding: 10px;
+margin: 5px;
+`;
+
+const Input  = styled.input`
+background-color: white;
+padding: 8px 8px;
+border-radius: 5px;
+outline: none;
+
+::placeholder {
+    font-size: 1.1em;
+    font-weight: lighter;
+    color: #999
+  }
+`;
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+              }
+        };
+        
+   
+    render() 
+    {
+        return (
+            <NavWrap> 
+            <Input></Input>
+            <IconWrapSearch><FontAwesomeIcon icon={faSearch} size="lg" /></IconWrapSearch>
+            <IconWrap><FontAwesomeIcon icon={faUser} size="lg" /></IconWrap>
+            <IconWrap><FontAwesomeIcon icon={faShoppingCart} size="lg" /></IconWrap>
+            </NavWrap>
+        )
+    }
+}
+    
+    export default Header;
