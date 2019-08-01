6d06cc030d31e069edaf91beb1f6045440e1fb22
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-08-01 19:56:48 +0200

    add header so it will be displayed on all sites

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Register from "./components/pages/Register";
 import Login from "./components/pages/Login";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
+    <div> 
+    <Header />
     <Router>
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
     </Router>
-    
+    </div>
   );
 }
 
