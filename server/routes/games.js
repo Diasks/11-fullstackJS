9d6a7274ed25a090146ec9d8ad952eee72f130e0
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-08-14 00:30:30 +0200

    get games from third-part api, put games in my own database

---
+++ server/routes/games.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const axios = require("axios");
+const config = require("dotenv").config();
+const mongoose = require("mongoose");
+
+
+router.get("/", (req, res, next)=> {
+axios.get("https://api.rawg.io/api/games")
+.then ((results) =>{
+    console.log(results);
+    // let gamesArray = [];
+let games = results.data.results;
+    debugger;
+    mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@gameover-uogeo.mongodb.net/test?retryWrites=true&w=majority`);
+debugger;
+    const db = mongoose.connection;
+debugger;
+        db.collection("games").insert(games, function(err, res) {
+            debugger;
+if(err) throw err;
+console.log(games);
+console.log(res);
+        })
+
+})
+.catch((err)=> {
+    console.log(err);
+});
+res.render("Games");
+})
+
+
+module.exports = router;
