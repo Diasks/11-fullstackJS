be620fb7fa251fd7975521f7b8d2f11692912322
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-08-15 15:08:33 +0200

    start with crud for user, get all users and delete specific user working

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-
 router.use(bodyParser.urlencoded( { extended: true } ));
 router.use(bodyParser.json());
-
+const VerifyToken = require('../middleware/VerifyToken');
 var User = require('../models/User');
 
+
+// GET ALL USERS
+router.get('/', VerifyToken, function(req, res) {
+    debugger;
+User.find({}, function(error, users) {
+    debugger;
+if(error) {
+    return res.status(500).send("an error occured")
+} else {
+    res.status(200).send(users);
+}
+
+})
+
+});
+
+//GET SPECIFIC USER -> DIDN'T USE YET
+router.get('/:id', VerifyToken, function(req, res) {
+    debugger;
+    User.findById(req.param.id, function(error, user) {
+    if(error) {
+        return res.status(500).send("an error occured")
+    } else {
+        res.status(200).send(user);
+    }
+    
+    })
+    
+    });
+
+      //TODO: UPDATE USER
+    router.patch("/:id", VerifyToken, function(req, res) {
+        debugger;
+        User.findByIdAndUpdate(
+            //put things here
+         
+          function(error, user) {
+            if (error) {
+              res.json({ status: "error", message: `${error}` });
+            }
+            res.status(200).json(user);
+          }
+        );
+      });
+
+
+
+//funkar med frontenden wohooo!!!
+  router.delete('/:id', VerifyToken, function (req, res) {
+      debugger;
+        User.findByIdAndRemove({_id: req.userId}, function(err, user){
+            debugger;
+            if(err) res.json(err);
+            else res.json('Successfully removed');
+        });
+    });
+
+
+    module.exports = router;
