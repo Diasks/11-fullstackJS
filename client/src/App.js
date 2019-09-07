7142d1de0f6262475ece2fe73a48a8d0179265ac
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-09-07 15:45:42 +0200

    remove shitload of unnecessary code, refactor everything, put all my state in one component instead and from there send state or recieve state, try to use async and get cleaner code

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Register from "./components/pages/Register";
 import Login from "./components/pages/Login";
+import Search from "./components/pages/Search";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+  state = {
+users: []
+  }
+
+//refaktorisera till async funktion för att söka i databasen
+searchUsers = async query => {
+  debugger;
+    const res = await axios.get(`http://localhost:4000/search/${query}`);
+  debugger;
+    this.setState({users: res.data.data })
+  }
+
+
+render() { 
+  //refaktorisera till att dekonstruera state för o ta ut spel från state
+  const { users } = this.state;
+
   return (
     <div> 
-    <Header />
+
     <Router>
+    <Header />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
-    </Router>
+        <Switch>
+    <Route 
+    exact 
+    path='/' render={props => (
+<Search 
+  searchUsers={this.searchUsers}/>
+    )} />
+    </Switch>
     <Footer />
+    </Router>
+   
     </div>
   );
-}
+}}
 
 export default App;
--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -1,23 +1,5 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
-
-const ObjectFrame  = styled.div`
-width: 100%;
-height: auto;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-`;
-
-const GameName   = styled.h1`
-text-align: center;
-width: 100%;
-font-size: 12px;
-color: #111;
-font-weight: lighter;
-`;
 
 export const GameImage = styled.img`
 width:100%;
@@ -52,127 +34,41 @@ border: 2px solid #519e8a;
 `;
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            results: [],
-            specificGame: null
-              }
-              this.showValue = this.showValue.bind(this);
-              this.doSomething = this.doSomething.bind(this);
-        };
-
-
-        showValue(e){
-            debugger;
-            e.preventDefault();
-            console.log(this.nameValue.value)
-            let query = this.nameValue.value;
-            axios.get(`http://localhost:4000/search/${query}`)
-            .then((data) => {
-              debugger;
-              console.log(data.data);
-              this.setState({results: data.data})
-              debugger;
-             
-            })
-        }
-
-         Suggestions = (props) => {
-            debugger;
-             if (props.results.data !== undefined && props.results.data.length === 0) {
-                debugger;
-    return (<p>no game</p>)
-            }
-             else if (props.results.data !== undefined) { 
-                debugger;
-            console.log(props.results.data)
-            const games = props.results.data.map((game, i) =>
-             (
-         <div className="box" key={i} > 
-              <ObjectFrame> 
+    state = {
+        query: ''
+            };
         
-    
-        
-           <GameName onClick={this.doSomething.bind(this, game)}>  {game.name} <button>hej!</button> </GameName>
-           <GameImage src={game.background_image} alt="games" />
-         
-          </ObjectFrame>
-          </div>
-             )
-         ) 
-        return (games)}
-    
-        else {
-            debugger;
-            return (
-                null);
-        }
+       
+        onChange = (e) => 
+        {
+            this.setState({[e.target.name]: e.target.value});
     }
 
-
-  doSomething(game) {
-      debugger;
-      this.setState({specificGame: game}, ()=> {
-          debugger;
-          console.log("blablabla");
-      })
-       }
-
-   
-
+        onSubmit = (e) => {
+            e.preventDefault();
+                this.props.searchUsers(this.state.query);
+            }
+    
+    
     render() { 
-if (this.state.specificGame != null) {
-    const game = this.state.specificGame;
 return ( 
     <div> 
-  <button onClick={() => this.props.history.goBack()}>Back</button>
-    <h2> {game.name}</h2>
-           <iframe title="preview" src={game.clip.clip}></iframe>
-           <p> Realese date: {game.released} </p> 
-           <ul>Genres:
-           {game.genres.map((genre, i) =>(
-
-            <li key={i}>{genre.name}</li>
-      
-        ))}      </ul>
-        
-        <ul> Platforms:
-        {game.platforms.map((p, i) =>(
- 
-            <li key={i}>{p.platform.name}</li>
-          
-        ))}   </ul>
-
-        {game.short_screenshots.map((s, i) => (
-            <div key={i}> <GameImage src={s.image} alt="screenshots" /></div>
-        ))}
+     
+                <form onSubmit={this.onSubmit} className="form">
+<Input type="text" name="query" placeholder="...search" value={this.state.query} onChange={this.onChange} />
+<Button type="submit">search</Button>
+                </form> 
            </div>
 )
 
 
 }
+
+}
   
 
 
-        debugger;
-        return ( 
-<div> 
 
 
-        }
-            <Input placeholder="search for.." ref={el=> this.nameValue=el}></Input> <Button onClick={this.showValue}>search</Button>
-          
-<div> 
- {this.state.results && 
-         <this.Suggestions results={this.state.results} />
-         }
-</div>
-
-        
-</div>
-         );
-    }
-}
  
 export default Search;
