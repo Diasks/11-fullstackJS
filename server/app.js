7eb870edf62e4f1e4d4a46d57bdafc63368e4460
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-09-18 00:57:43 +0200

    try to fix heroku deploy

--- server/app.js
+++ server/app.js
@@ -57,14 +57,25 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 
-app.get("*", function(request, response) {
-  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+
+// app.use(express.static(path.join(__dirname, "../client/build")));
+
+
+// app.get("*", function(request, response) {
+//   response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// });
 
 
 
