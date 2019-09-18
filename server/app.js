c59c22e973d49d39dad638e9cc307026f2a7bc2f
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-09-18 15:16:49 +0200

    error request to backend not working

--- server/app.js
+++ server/app.js
@@ -59,23 +59,19 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, '../client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
 
+// Handle React routing, return all requests to React app
+  // app.get('*', function(req, res) {
+  //   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  // });
 
 
 // app.use(express.static(path.join(__dirname, "../client/build")));
 
 
-// app.get("*", function(request, response) {
-//   response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-// });
+app.get("*", function(request, response) {
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 
 
