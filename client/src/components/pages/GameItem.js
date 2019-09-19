cb024979c56b1c55a801375beef6d5046f059c11
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-09-19 22:26:21 +0200

    fix image error that show in deploy

--- client/src/components/pages/GameItem.js
+++ client/src/components/pages/GameItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Box = styled.div `
+const Box = styled.div`
 width: 40%;
 height: 150px;
 margin: 10px;
@@ -13,18 +13,21 @@ display:flex;
 background-position: center;
 background-size: 200px 200px;
 background-repeat: no-repeat;
+
 @media (min-width: 501px) {
     height: 200px;
     background-position: center;
     background-size: 500px 200px;
-    background-repeat: no-repeat; }
+    background-repeat: no-repeat; 
+}
     @media (min-width:  1250px) {
         height: 350px;
         background-position: center;
         background-size: 800px 350px;
-        background-repeat: no-repeat; }
-     
+        background-repeat: no-repeat; 
+}
   `;
+
 
 const StyledLink = styled(Link)`
 align-self: flex-end;
@@ -44,7 +47,7 @@ function GameItem({game: {name, background_image, slug}}) {
     debugger;
     return (
         <Box style={{backgroundImage: `url(${background_image})`}}>
-            <StyledLink to={`/game/${slug}`}> {name}</StyledLink>
+            <StyledLink to={`/game/${slug}`}>{name}</StyledLink>
             </Box>
     )
 }
